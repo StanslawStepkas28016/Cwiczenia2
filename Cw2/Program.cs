02ef52ffabe2ae715d8b0312e7098037117a220d
s28016 <s28016@pjwstk.edu.pl>
2024-03-11 09:53:40 +0100

    Dodana metoda average, wyliczająca średnią liczb z tablicy.

--- Cw2/Program.cs
+++ Cw2/Program.cs
@@ -3,7 +3,18 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello!");
-        Console.WriteLine("Hello 2!");
-        Console.WriteLine("Hello 3!");
+        Console.WriteLine(average(new[] { 1, 2, 3}));
+    }
+
+    public static double average(int[] arr)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        return sum / (double)(arr.Length);
     }
 }
