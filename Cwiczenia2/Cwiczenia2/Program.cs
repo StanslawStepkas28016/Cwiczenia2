c7c0645e93b15e6dc0fc28e5c61b15f80f809350
s28016 <s28016@pjwstk.edu.pl>
2024-03-11 14:22:37 +0100

    Poprawka projektu

---
+++ Cwiczenia2/Cwiczenia2/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello!");
+        Console.WriteLine(average(new[] { 1, 2, 3 }));
+    }
+
+    public static double average(int[] arr)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        return sum / (double)(arr.Length);
+    }
+
+    public static int max(int[] arr)
+    {
+        return 1;
+    }
+}
